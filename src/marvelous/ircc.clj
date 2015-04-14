dfa2219d0748d7656481432579411eb0f7fc4b53
andrerocker <andre.souza@gmail.com>
2015-04-14 14:16:45 -0300

    ircc small refactory

--- src/marvelous/ircc.clj
+++ src/marvelous/ircc.clj
@@ -4,18 +4,24 @@
            (java.io InputStreamReader BufferedReader PrintWriter))
   (:gen-class))
 
-(defn send-payload [output properties]
-  (println "Starting Marvelous IRC Bot :p")
+(defn- send-command [output command]
   (doto output
-    (.println (str "NICK " (:nickname properties) "\r"))
-    (.println (str "USER " (:nickname properties) " 0 * : " (:nickname properties) "\r"))
+    (.println (str command "\r"))
     (.flush)))
+
+(defn- send-payload [output properties]
+    (send-command output (str "NICK " (:nickname properties)))
+    (send-command output (str "USER " (:nickname properties) " 0 * : " (:nickname properties))))
+
+(defn- start-loop [input output properties]
+    (while-let [line (.readLine input)]
+      (println line)))
 
 (defn start-ircc [properties]
   (let [socket (Socket. (:server properties) (:port properties))
         input (BufferedReader. (InputStreamReader. (.getInputStream socket)))
         output (PrintWriter. (.getOutputStream socket))]
-
+  
     (send-payload output properties)
-    (while-let [line (.readLine input)]
-               (println line))))
+    (println "Starting Marvelous IRC Bot :p")
+    (start-loop input output properties)))
