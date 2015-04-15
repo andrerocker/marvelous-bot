03e24c73afa98261cb109f551af0feaead5afbeb
andrerocker <andre.souza@gmail.com>
2015-04-15 20:26:15 -0300

    update project.clj
    - change version to 0.0.1 :D
    - add project url
    - set target-dir to dist
    - update description

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject marvelous "1.0.0-SNAPSHOT"
-  :description "A IRC super bot :p"
+(defproject marvelous "0.0.1-SNAPSHOT"
+  :description "Marvelous - A IRC super bot :p"
+  :url "http://github.com/andrerocker/marvelous-bot"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [while-let "0.2.0"]]
+  :target-dir "dist"
   :main marvelous.main)
