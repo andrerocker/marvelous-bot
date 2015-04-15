4f105dd8fec2d38c4557fa1ac0a7baeb04d72c4e
andrerocker <andre.souza@gmail.com>
2015-04-14 23:29:22 -0300

    extract commands to sets

--- src/marvelous/ircc.clj
+++ src/marvelous/ircc.clj
@@ -5,6 +5,7 @@
   (:gen-class))
 
 (defn- send-command [output command]
+  (println (str ">>> " command))
   (doto output
     (.println (str command "\r"))
     (.flush)))
@@ -13,15 +14,20 @@
     (send-command output (str "NICK " (:nickname properties)))
     (send-command output (str "USER " (:nickname properties) " 0 * : " (:nickname properties))))
 
+(def log { :matcher #(boolean %) :handler-fn #(println %) })
+(def ping { :matcher #(re-find #"PING" %) :handler-fn #(str "PONG" %) })
+
 (defn- start-loop [input output properties]
     (while-let [line (.readLine input)]
-      (println line)))
+      (println (str "<<< " line))
+        (if ((:matcher ping) line)
+          (send-command output ((:handler-fn ping) line)))))
 
 (defn start-ircc [properties]
   (let [socket (Socket. (:server properties) (:port properties))
         input (BufferedReader. (InputStreamReader. (.getInputStream socket)))
         output (PrintWriter. (.getOutputStream socket))]
-  
+
+    (println " ~~~~~~~~~~~~~~~~~~ Starting Marvelous IRC Bot :p ~~~~~~~~~~~~~~~\n")
     (send-payload output properties)
-    (println "Starting Marvelous IRC Bot :p")
     (start-loop input output properties)))
