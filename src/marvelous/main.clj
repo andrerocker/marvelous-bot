14f3e9ca0af6ad267d4fa310529bfe0ef3cf2748
andrerocker <andre.souza@gmail.com>
2015-04-15 18:17:48 -0300

    remove non-used requirements

--- src/marvelous/main.clj
+++ src/marvelous/main.clj
@@ -1,8 +1,6 @@
 (ns marvelous.main
   (:require [marvelous.args :refer [parse-args]]
             [marvelous.ircc :refer [start-ircc]])
-  (:import (java.net Socket)
-           (java.io InputStreamReader BufferedReader PrintWriter))
   (:gen-class))
 
 (defn -main [& args]
