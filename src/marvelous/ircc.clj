20a27b5eeb24d8dcf20ae615f014b62307059c8c
andrerocker <andre.souza@gmail.com>
2015-04-15 16:37:38 -0300

    first version of a dynamic/registered plugin system

--- src/marvelous/ircc.clj
+++ src/marvelous/ircc.clj
@@ -14,20 +14,27 @@
     (send-command output (str "NICK " (:nickname properties)))
     (send-command output (str "USER " (:nickname properties) " 0 * : " (:nickname properties))))
 
-(def log { :matcher #(boolean %) :handler-fn #(println %) })
-(def ping { :matcher #(re-find #"PING" %) :handler-fn #(str "PONG" %) })
+(def log { :matcher (fn[line](boolean true)) :handler-fn #(println "<<<" %) })
+(def ping { :matcher #(re-find #"PING" %) :handler-fn #(str "PONG " %) })
+
+(defn- select-plugins [plugins line]
+  (filter #((:matcher %) line) plugins))
+
+(defn- execute-plugin [output plugin line]
+  (let [response ((:handler-fn plugin) line)]
+    (if response
+      (send-command output response))))
 
 (defn- start-loop [input output properties]
-    (while-let [line (.readLine input)]
-      (println (str "<<< " line))
-        (if ((:matcher ping) line)
-          (send-command output ((:handler-fn ping) line)))))
+  (while-let [line (.readLine input)]
+    (doseq [current (select-plugins #{log ping} line)]
+      (execute-plugin output current line))))
 
 (defn start-ircc [properties]
+  (println " ~~~~~~~~~~~~~~~~~~ Starting Marvelous IRC Bot :p ~~~~~~~~~~~~~~~\n")
   (let [socket (Socket. (:server properties) (:port properties))
         input (clojure.java.io/reader (.getInputStream socket))
         output (PrintWriter. (.getOutputStream socket))]
 
-    (println " ~~~~~~~~~~~~~~~~~~ Starting Marvelous IRC Bot :p ~~~~~~~~~~~~~~~\n")
     (send-payload output properties)
     (start-loop input output properties)))
