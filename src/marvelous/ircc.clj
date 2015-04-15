b7fc813dfc029a991511d4077ad1e6c4d55adb40
andrerocker <andre.souza@gmail.com>
2015-04-15 19:32:44 -0300

    use java Executors \o/ to handle the execution os matched plugins

--- src/marvelous/ircc.clj
+++ src/marvelous/ircc.clj
@@ -2,7 +2,8 @@
   (:require [while-let.core :refer [while-let]]
             [marvelous.plug :refer [core-plugins]])
   (:import (java.net Socket)
-           (java.io InputStreamReader BufferedReader PrintWriter))
+           (java.io PrintWriter)
+           (java.util.concurrent Executors))
   (:gen-class))
 
 (defn- send-command [output command]
@@ -26,16 +27,17 @@
   (if-let [response ((:handler-fn plugin) line)]
     (send-command output response)))
 
-(defn- start-loop [input output properties]
+(defn- start-loop [executor input output properties]
   (while-let [line (.readLine input)]
     (doseq [current (select-plugins line)]
-      (execute-plugin output current line))))
+      (.submit executor #(execute-plugin output current line)))))
 
 (defn start-ircc [properties]
   (println " ~~~~~~~~~~~~~~~~~~ Starting Marvelous IRC Bot :p ~~~~~~~~~~~~~~~\n")
   (let [socket (Socket. (:server properties) (:port properties))
         input (clojure.java.io/reader (.getInputStream socket))
-        output (PrintWriter. (.getOutputStream socket))]
+        output (PrintWriter. (.getOutputStream socket))
+        executor (Executors/newCachedThreadPool)]
 
     (send-payload output properties)
-    (start-loop input output properties)))
+    (start-loop executor input output properties)))
