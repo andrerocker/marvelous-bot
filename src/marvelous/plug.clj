3af0e821a97d88e00ba3517df8e845da3cedd5a8
andrerocker <andre.souza@gmail.com>
2015-04-16 01:14:21 -0300

    use fn short mode on plugs

--- src/marvelous/plug.clj
+++ src/marvelous/plug.clj
@@ -1,8 +1,8 @@
 (ns marvelous.plug
   (:gen-class))
 
-(def log { :matcher (fn[line](boolean true)) :handler-fn #(println "<<<" %) })
-(def ping { :matcher #(re-find #"PING" %) :handler-fn (fn[line](str "PONG")) })
+(def log { :matcher (fn[line]true) :handler-fn #(println "<<<" %) })
+(def ping { :matcher #(re-find #"PING" %) :handler-fn (fn[line]"PONG") })
 
 (defn core-plugins []
   [log ping])
