1b98ae691c6a6eaac6ebde1cce8146c65c321c04
andrerocker <andre.souza@gmail.com>
2015-04-17 03:19:41 -0300

    insonia specs

--- spec/marvelous/plug_spec.clj
+++ spec/marvelous/plug_spec.clj
@@ -7,4 +7,27 @@
       (let [plugins (core-plugins)]
         (should= [log ping] plugins))))
 
+(describe "#log"
+  (it "match all lines"
+      (let [matcher (:matcher log)]
+        (should= true (matcher false))
+        (should= true (matcher true))
+        (should= true (matcher nil))
+        (should= true (matcher ""))))
+
+  (it "log line and return nil"
+      (should-invoke println {:with ["<<<" "bacon"] :return nil}
+        (let [result ((:handler-fn log) "bacon")]
+          (should-be-nil result)))))
+
+(describe "#ping"
+  (it "matches ping command"
+      (let [matcher (:matcher ping)]
+        (should-not-be-nil (matcher "blabla PING blabla"))
+        (should-be-nil (matcher "lalala XXXX ldsllsa"))))
+
+  (it "says PONG"
+      (let [handler-fn (:handler-fn ping)]
+        (should= "PONG" (handler-fn "kdoaskd kdoas sakdo")))))
+
 (run-specs)
